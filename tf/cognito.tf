3468b670c3c20b79006e87b4c462e96781dca0b8
Nick Doyle <nick.doyle@intelematics.com>
2021-02-19 11:23:35 +1100

    feat: enable mfa, relax password reqs

--- tf/cognito.tf
+++ tf/cognito.tf
@@ -5,4 +5,29 @@ resource "aws_cognito_user_pool_domain" "main" {
 
 resource "aws_cognito_user_pool" "customer" {
   name = "customer"
+
+  mfa_configuration = "ON"
+  software_token_mfa_configuration {
+    enabled = true
+  }
+
+  account_recovery_setting {
+    recovery_mechanism {
+      name     = "verified_email"
+      priority = 1
+    }
+
+    recovery_mechanism {
+      name     = "verified_phone_number"
+      priority = 2
+    }
+  }
+
+  password_policy {
+    minimum_length    = 6
+    require_lowercase = false
+    require_uppercase = false
+    require_symbols = false
+    require_numbers = false
+  }
 }
